6bf72c1c40f9af87ded0d15bfea575965f9f6090
radioActive DROID <me@godwin.dev>
2021-06-04 21:20:09 +0100

    Change project structure to be standard

--- titan/__main__.py
+++ titan/__main__.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
-
 #  Copyright 2021 by Godwin peter <me@godwin.dev>, Tolaram Group.
 #  All rights reserved.
 #  This file is part of the the Titan micro task scheduler project,
 #  and is released under the "MIT License Agreement". Please see the LICENSE
 #  file that should have been included as part of this package.
 
-def main():
-    print('Start project..')
+from titan import app
 
+def main():
+    if __name__ == '__main__':
+        app.run()
 
 main()
