066bf42ca98018d309294e249373839ecbefd0d7
radioActive DROID <me@godwin.dev>
2021-06-07 18:37:40 +0100

    Create logs utils

---
+++ titan/common/utils/log.py
@@ -0,0 +1,37 @@
+#   Copyright 2021 by Godwin peter <me@godwin.dev>, Tolaram Group.
+#   All rights reserved.
+#   This file is part of the the Titan micro task scheduler project,
+#   and is released under the "MIT License Agreement". Please see the LICENSE
+#   file that should have been included as part of this package.
+
+import logging
+import logging.handlers
+import sys
+
+
+def get():
+    # Change root logger level from WARNING (default) to NOTSET in order for all messages to be delegated.
+    logging.getLogger().setLevel(logging.NOTSET)
+
+    # Add stdout handler, with level INFO
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(name)-13s: %(levelname)-8s %(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+
+    # Add file rotating handler, with level DEBUG
+    rotating_handler = logging.handlers.RotatingFileHandler(
+        filename="logs/app.log", maxBytes=1000, backupCount=5
+    )
+    rotating_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    rotating_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(rotating_handler)
+
+    return logging.getLogger(__name__)
+
+
+logger = get()
