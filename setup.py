2bc6a53ee5e8a0036f14d2eaf9f3d3df9b8a41fa
radioActive DROID <me@godwin.dev>
2021-06-07 10:04:51 +0100

    Lint setup file

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='Titan',
-      version='0.01',
-      description='An easy to use python background task scheduler',
-      long_description=read('README.md'),
-      author='Godwin peter .O',
-      author_email='me@godwin.dev',
-      url='hhttps://github.com/TolaramGroup/titanProject/',
-      license='MIT',
-      platforms=['windows', 'osx', 'linux'],
-      packages=['titan', 'titan.command'],
-      classifiers=[
+setup(
+    name="titan",
+    version="0.01",
+    description="An easy to use python background task scheduler",
+    long_description=read("README.md"),
+    author="Godwin peter .O",
+    author_email="me@godwin.dev",
+    url="hhttps://github.com/TolaramGroup/titanProject/",
+    license="MIT",
+    platforms=["windows", "osx", "linux"],
+    packages=["titan", "titan.command"],
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
-      ],
-      )
+    ],
+)
