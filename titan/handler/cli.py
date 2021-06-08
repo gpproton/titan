8278d8ab3bf582222c1f932c0d5c3f77e905b5f0
radioActive DROID <me@godwin.dev>
2021-06-08 17:32:02 +0100

    create CLI handler

---
+++ titan/handler/cli.py
@@ -0,0 +1,15 @@
+#   Copyright 2021 by Godwin peter <me@godwin.dev>, Tolaram Group.
+#   All rights reserved.
+#   This file is part of the the Titan micro task scheduler project,
+#   and is released under the "MIT License Agreement". Please see the LICENSE
+#   file that should have been included as part of this package.
+
+import click
+
+
+@click.group(
+    help="Titan, easy-to-use python background task scheduler",
+    invoke_without_command=True,
+)
+def hook():
+    pass
