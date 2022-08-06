d8b6edc79d1fe5194ce88c56adc0612dcfedd938
radioActive DROID <me@godwin.dev>
2022-08-06 23:40:32 +0100

    Fix module name

--- titan/__init__.py
+++ titan/__init__.py
@@ -3,8 +3,9 @@
 #   This file is part of the the Titan micro task scheduler project,
 #   and is released under the "MIT License Agreement". Please see the LICENSE
 #   file that should have been included as part of this package.
+
 from dependency_injector.wiring import inject
-from titan.App import App
+from titan.app import App
 
 
 @inject
